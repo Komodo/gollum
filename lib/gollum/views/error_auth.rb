9afcb7ee0ed8d562e7927df86395dc0fdf195544
Nathan Rijksen <n.rijksen@gmail.com>
2015-08-18 11:35:38 -0400

    Fix caps

--- lib/gollum/views/error_auth.rb
+++ lib/gollum/views/error_auth.rb
@@ -2,7 +2,7 @@ module Omnigollum
   module Views
     class Error_auth < Mustache
       self.template_path = File.expand_path("../../templates", __FILE__)
-      self.template_name = 'Error_auth'
+      self.template_name = 'error_auth'
       
       def title
         @title
